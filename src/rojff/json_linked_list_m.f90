dbf8c4e19be744677ee024b975e2dcd53c8d7186
Brad Richardson <everythingfunctional@protonmail.com>
2022-09-21 18:33:52 -0500

    refac(linked_list): split into module and submodule

--- src/rojff/json_linked_list_m.f90
+++ src/rojff/json_linked_list_m.f90
@@ -1,5 +1,5 @@
 module rojff_json_linked_list_m
-    use rojff_json_element_m, only: json_element_t, move_into_element
+    use rojff_json_element_m, only: json_element_t
     use rojff_json_value_m, only: json_value_t
 
     implicit none
@@ -20,38 +20,18 @@ module rojff_json_linked_list_m
         procedure :: append
         procedure :: move_into_elements
     end type
-contains
-    subroutine append(self, json)
-        class(json_linked_list_t), intent(inout) :: self
-        class(json_value_t), allocatable, intent(inout) :: json
 
-        if (self%size == 0) then
-            allocate(self%head)
-            self%tail => self%head
-            call move_alloc(json, self%head%json)
-            self%size = 1
-        else
-            allocate(self%tail%next)
-            self%tail => self%tail%next
-            call move_alloc(json, self%tail%json)
-            self%size = self%size + 1
-        end if
-    end subroutine
+    interface
+        module subroutine append(self, json)
+            implicit none
+            class(json_linked_list_t), intent(inout) :: self
+            class(json_value_t), allocatable, intent(inout) :: json
+        end subroutine
 
-    subroutine move_into_elements(self, elements)
-        class(json_linked_list_t), intent(inout) :: self
-        type(json_element_t), allocatable, intent(out) :: elements(:)
-
-        type(json_node_t), pointer :: curr
-        integer :: i
-
-        allocate(elements(self%size))
-        curr => self%head
-        do i = 1, self%size
-            call move_into_element(elements(i), curr%json)
-            curr => curr%next
-            deallocate(self%head)
-            self%head => curr
-        end do
-    end subroutine
+        module subroutine move_into_elements(self, elements)
+            implicit none
+            class(json_linked_list_t), intent(inout) :: self
+            type(json_element_t), allocatable, intent(out) :: elements(:)
+        end subroutine
+    end interface
 end module
