23d8a13827931b284ca596d3b1b54345482375ba
Brad Richardson <everythingfunctional@protonmail.com>
2022-09-21 18:28:05 -0500

    refac(json_integer): split into module and submodule

--- src/rojff/json_integer_m.f90
+++ src/rojff/json_integer_m.f90
@@ -15,56 +15,38 @@ module rojff_json_integer_m
     end type
 
     interface json_integer_t
-        module procedure constructor
+        elemental module function constructor(number) result(json_integer)
+            implicit none
+            type(json_integer_t) :: json_integer
+            integer, intent(in) :: number
+        end function
     end interface
-contains
-    elemental function constructor(number) result(json_integer)
-        type(json_integer_t) :: json_integer
-        integer, intent(in) :: number
 
-        json_integer%number = number
-    end function
+    interface
+        module subroutine create_json_integer(json, number)
+            implicit none
+            class(json_value_t), allocatable, intent(out) :: json
+            integer, intent(in) :: number
+        end subroutine
 
-    subroutine create_json_integer(json, number)
-        class(json_value_t), allocatable, intent(out) :: json
-        integer, intent(in) :: number
+        elemental module function equals(lhs, rhs)
+            implicit none
+            class(json_integer_t), intent(in) :: lhs
+            class(json_value_t), intent(in) :: rhs
+            logical :: equals
+        end function
 
-        type(json_integer_t), allocatable :: local
+        module subroutine write_to_compactly(self, sink)
+            implicit none
+            class(json_integer_t), intent(in) :: self
+            class(string_sink_t), intent(inout) :: sink
+        end subroutine
 
-        allocate(local)
-        local%number = number
-        call move_alloc(local, json)
-    end subroutine
-
-    elemental function equals(lhs, rhs)
-        class(json_integer_t), intent(in) :: lhs
-        class(json_value_t), intent(in) :: rhs
-        logical :: equals
-
-        select type (rhs)
-        type is (json_integer_t)
-            equals = lhs%number == rhs%number
-        class default
-            equals = .false.
-        end select
-    end function
-
-    subroutine write_to_compactly(self, sink)
-        class(json_integer_t), intent(in) :: self
-        class(string_sink_t), intent(inout) :: sink
-
-        character(len=11) :: temp
-
-        write(temp, '(I0)') self%number
-        call sink%append(trim(temp))
-    end subroutine
-
-    subroutine write_to_expanded(self, indentation_level, sink)
-        class(json_integer_t), intent(in) :: self
-        integer, intent(in) :: indentation_level
-        class(string_sink_t), intent(inout) :: sink
-
-        associate(unused => indentation_level); end associate
-        call self%write_to_compactly(sink)
-    end subroutine
+        module subroutine write_to_expanded(self, indentation_level, sink)
+            implicit none
+            class(json_integer_t), intent(in) :: self
+            integer, intent(in) :: indentation_level
+            class(string_sink_t), intent(inout) :: sink
+        end subroutine
+    end interface
 end module
---
+++ src/rojff/json_integer_s.f90
@@ -0,0 +1,36 @@
+submodule(rojff_json_integer_m) rojff_json_integer_s
+    implicit none
+contains
+    module procedure constructor
+        json_integer%number = number
+    end procedure
+
+    module procedure create_json_integer
+        type(json_integer_t), allocatable :: local
+
+        allocate(local)
+        local%number = number
+        call move_alloc(local, json)
+    end procedure
+
+    module procedure equals
+        select type (rhs)
+        type is (json_integer_t)
+            equals = lhs%number == rhs%number
+        class default
+            equals = .false.
+        end select
+    end procedure
+
+    module procedure write_to_compactly
+        character(len=11) :: temp
+
+        write(temp, '(I0)') self%number
+        call sink%append(trim(temp))
+    end procedure
+
+    module procedure write_to_expanded
+        associate(unused => indentation_level); end associate
+        call self%write_to_compactly(sink)
+    end procedure
+end submodule
