8dc4fd3b70bf4e3aba34d9ea91744bf408260761
Brad Richardson <everythingfunctional@protonmail.com>
2022-09-21 18:59:48 -0500

    refac(json_member): split into module and submodule

--- src/rojff/json_member_m.f90
+++ src/rojff/json_member_m.f90
@@ -1,5 +1,5 @@
 module rojff_json_member_m
-    use iso_varying_string, only: varying_string, char
+    use iso_varying_string, only: varying_string
     use rojff_json_element_m, only: json_element_t
     use rojff_json_string_m, only: json_string_t
     use rojff_json_value_m, only: json_value_t
@@ -24,113 +24,91 @@ module rojff_json_member_m
     end type
 
     interface json_member_t
-        module procedure construct_v
-        module procedure construct_e
+        impure elemental module function construct_v( &
+                key, value_) result(json_member)
+            implicit none
+            type(json_string_t), intent(in) :: key
+            class(json_value_t), intent(in) :: value_
+            type(json_member_t) :: json_member
+        end function
+
+        impure elemental module function construct_e( &
+                key, element) result(json_member)
+            implicit none
+            type(json_string_t), intent(in) :: key
+            type(json_element_t), intent(in) :: element
+            type(json_member_t) :: json_member
+        end function
     end interface
 
     interface json_member_unsafe
-        module procedure json_member_unsafe_cv
-        module procedure json_member_unsafe_ce
-        module procedure json_member_unsafe_sv
-        module procedure json_member_unsafe_se
+        impure elemental module function json_member_unsafe_cv( &
+                key, value_) result(json_member)
+            implicit none
+            character(len=*), intent(in) :: key
+            class(json_value_t), intent(in) :: value_
+            type(json_member_t) :: json_member
+        end function
+
+        impure elemental module function json_member_unsafe_ce( &
+                key, element) result(json_member)
+            implicit none
+            character(len=*), intent(in) :: key
+            type(json_element_t), intent(in) :: element
+            type(json_member_t) :: json_member
+        end function
+
+        impure elemental module function json_member_unsafe_sv( &
+                key, value_) result(json_member)
+            implicit none
+            type(varying_string), intent(in) :: key
+            class(json_value_t), intent(in) :: value_
+            type(json_member_t) :: json_member
+        end function
+
+        impure elemental module function json_member_unsafe_se( &
+                key, element) result(json_member)
+            implicit none
+            type(varying_string), intent(in) :: key
+            type(json_element_t), intent(in) :: element
+            type(json_member_t) :: json_member
+        end function
     end interface
-contains
-    impure elemental function json_member_unsafe_cv(key, value_) result(json_member)
-        character(len=*), intent(in) :: key
-        class(json_value_t), intent(in) :: value_
-        type(json_member_t) :: json_member
 
-        json_member%key = key
-        json_member%value_ = value_
-    end function
+    interface
+        module subroutine move_into_member(member, key, value_)
+            implicit none
+            type(json_member_t), intent(out) :: member
+            type(json_string_t), intent(inout) :: key
+            class(json_value_t), allocatable, intent(inout) :: value_
+        end subroutine
 
-    impure elemental function json_member_unsafe_ce(key, element) result(json_member)
-        character(len=*), intent(in) :: key
-        type(json_element_t), intent(in) :: element
-        type(json_member_t) :: json_member
+        module subroutine move_into_member_unsafe(member, key, value_)
+            implicit none
+            type(json_member_t), intent(out) :: member
+            character(len=*), intent(in) :: key
+            class(json_value_t), allocatable, intent(inout) :: value_
+        end subroutine
 
-        json_member%key = key
-        json_member%value_ = element%json
-    end function
+        recursive elemental module function equals(lhs, rhs)
+            implicit none
+            class(json_member_t), intent(in) :: lhs
+            type(json_member_t), intent(in) :: rhs
+            logical :: equals
+        end function
 
-    impure elemental function json_member_unsafe_sv(key, value_) result(json_member)
-        type(varying_string), intent(in) :: key
-        class(json_value_t), intent(in) :: value_
-        type(json_member_t) :: json_member
+        recursive module subroutine write_to_compactly(self, sink)
+            implicit none
+            class(json_member_t), intent(in) :: self
+            class(string_sink_t), intent(inout) :: sink
+        end subroutine
 
-        json_member%key = char(key)
-        json_member%value_ = value_
-    end function
-
-    impure elemental function json_member_unsafe_se(key, element) result(json_member)
-        type(varying_string), intent(in) :: key
-        type(json_element_t), intent(in) :: element
-        type(json_member_t) :: json_member
-
-        json_member%key = char(key)
-        json_member%value_ = element%json
-    end function
-
-    impure elemental function construct_v(key, value_) result(json_member)
-        type(json_string_t), intent(in) :: key
-        class(json_value_t), intent(in) :: value_
-        type(json_member_t) :: json_member
-
-        json_member%key = key%string
-        json_member%value_ = value_
-    end function
-
-    impure elemental function construct_e(key, element) result(json_member)
-        type(json_string_t), intent(in) :: key
-        type(json_element_t), intent(in) :: element
-        type(json_member_t) :: json_member
-
-        json_member%key = key%string
-        json_member%value_ = element%json
-    end function
-
-    subroutine move_into_member(member, key, value_)
-        type(json_member_t), intent(out) :: member
-        type(json_string_t), intent(inout) :: key
-        class(json_value_t), allocatable, intent(inout) :: value_
-
-        call move_alloc(key%string, member%key)
-        call move_alloc(value_, member%value_)
-    end subroutine
-
-    subroutine move_into_member_unsafe(member, key, value_)
-        type(json_member_t), intent(out) :: member
-        character(len=*), intent(in) :: key
-        class(json_value_t), allocatable, intent(inout) :: value_
-
-        member%key = key
-        call move_alloc(value_, member%value_)
-    end subroutine
-
-    recursive elemental function equals(lhs, rhs)
-        class(json_member_t), intent(in) :: lhs
-        type(json_member_t), intent(in) :: rhs
-        logical :: equals
-
-        equals = lhs%key == rhs%key .and. lhs%value_ == rhs%value_
-    end function
-
-    recursive subroutine write_to_compactly(self, sink)
-        class(json_member_t), intent(in) :: self
-        class(string_sink_t), intent(inout) :: sink
-
-        call sink%append('"')
-        call sink%append(self%key)
-        call sink%append('":')
-        call self%value_%write_to_compactly(sink)
-    end subroutine
-
-    recursive subroutine write_to_expanded(self, indentation_level, sink)
-        class(json_member_t), intent(in) :: self
-        integer, intent(in) :: indentation_level
-        class(string_sink_t), intent(inout) :: sink
-
-        call sink%append('"' // self%key // '" : ')
-        call self%value_%write_to_expanded(indentation_level, sink)
-    end subroutine
+        recursive module subroutine write_to_expanded( &
+                self, indentation_level, sink)
+            implicit none
+            class(json_member_t), intent(in) :: self
+            integer, intent(in) :: indentation_level
+            class(string_sink_t), intent(inout) :: sink
+        end subroutine
+    end interface
 end module
