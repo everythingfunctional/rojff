f125277cdc259dff3b81fd6910c3b7d89646b0ff
Brad Richardson <everythingfunctional@protonmail.com>
2022-09-21 16:04:35 -0500

    refac(fallible_json_string): split into module and submodule

--- src/rojff/fallible_json_string_m.f90
+++ src/rojff/fallible_json_string_m.f90
@@ -1,11 +1,7 @@
 module rojff_fallible_json_string_m
-    use erloff, only: error_list_t, fatal_t, module_t, procedure_t
-    use iso_varying_string, only: varying_string, char
-    use rojff_json_string_m, only: json_string_t, json_string_unsafe
-    use rojff_json_value_m, only: json_value_t
-    use rojff_parser_m, only: parse_json_string
-    use rojff_string_cursor_m, only: string_cursor_t
-    use rojff_utils_m, only: INVALID_INPUT
+    use erloff, only: error_list_t, module_t, procedure_t
+    use iso_varying_string, only: varying_string
+    use rojff_json_string_m, only: json_string_t
 
     implicit none
     private
@@ -19,76 +15,41 @@ module rojff_fallible_json_string_m
     end type
 
     interface fallible_json_string_t
-        module procedure from_character
-        module procedure from_string
-        module procedure from_json_string
-        module procedure from_fallible_string
+        module function from_character(string) result(fallible_string)
+            implicit none
+            character(len=*), intent(in) :: string
+            type(fallible_json_string_t) :: fallible_string
+        end function
+
+        impure elemental module function from_string( &
+                string) result(fallible_string)
+            implicit none
+            type(varying_string), intent(in) :: string
+            type(fallible_json_string_t) :: fallible_string
+        end function
+
+        impure elemental module function from_json_string( &
+                string) result(fallible_string)
+            implicit none
+            type(json_string_t), intent(in) :: string
+            type(fallible_json_string_t) :: fallible_string
+        end function
+
+        impure elemental module function from_fallible_string( &
+                maybe_string, module_, procedure_) result(fallible_string)
+            implicit none
+            type(fallible_json_string_t), intent(in) :: maybe_string
+            type(module_t), intent(in) :: module_
+            type(procedure_t), intent(in) :: procedure_
+            type(fallible_json_string_t) :: fallible_string
+        end function
     end interface
 
-    character (len=*), parameter :: MODULE_NAME = "rojff_fallible_json_string_m"
-contains
-    function from_character(string) result(fallible_string)
-        character(len=*), intent(in) :: string
-        type(fallible_json_string_t) :: fallible_string
-
-        character(len=*), parameter :: PROCEDURE_NAME = "from_character"
-        type(string_cursor_t) :: cursor
-        class(json_value_t), allocatable :: json
-
-        cursor = string_cursor_t(string // '"')
-        call parse_json_string(cursor, json, fallible_string%errors)
-        if (fallible_string%errors%has_any()) then
-            fallible_string%errors = error_list_t( &
-                    fallible_string%errors, &
-                    module_t(MODULE_NAME), &
-                    procedure_t(PROCEDURE_NAME))
-        else if (.not.cursor%finished()) then
-            fallible_string%errors = error_list_t(fatal_t( &
-                    INVALID_INPUT, &
-                    module_t(MODULE_NAME), &
-                    procedure_t(PROCEDURE_NAME), &
-                    "Unescaped quote in string: '" // string // "'"))
-        else
-            fallible_string%string = json_string_unsafe(string)
-        end if
-    end function
-
-    function from_string(string) result(fallible_string)
-        type(varying_string), intent(in) :: string
-        type(fallible_json_string_t) :: fallible_string
-
-        fallible_string = fallible_json_string_t( &
-                fallible_json_string_t(char(string)), &
-                module_t(MODULE_NAME), &
-                procedure_t("from_string"))
-    end function
-
-    function from_json_string(string) result(fallible_string)
-        type(json_string_t), intent(in) :: string
-        type(fallible_json_string_t) :: fallible_string
-
-        fallible_string%string = string
-    end function
-
-    function from_fallible_string( &
-            maybe_string, module_, procedure_) result(fallible_string)
-        type(fallible_json_string_t), intent(in) :: maybe_string
-        type(module_t), intent(in) :: module_
-        type(procedure_t), intent(in) :: procedure_
-        type(fallible_json_string_t) :: fallible_string
-
-        if (maybe_string%failed()) then
-            fallible_string%errors = error_list_t( &
-                    maybe_string%errors, module_, procedure_)
-        else
-            fallible_string%string = maybe_string%string
-        end if
-    end function
-
-    elemental function failed(self)
-        class(fallible_json_string_t), intent(in) :: self
-        logical :: failed
-
-        failed = self%errors%has_any()
-    end function
+    interface
+        elemental module function failed(self)
+            implicit none
+            class(fallible_json_string_t), intent(in) :: self
+            logical :: failed
+        end function
+    end interface
 end module
---
+++ src/rojff/fallible_json_string_s.f90
@@ -0,0 +1,60 @@
+submodule(rojff_fallible_json_string_m) rojff_fallible_json_string_s
+    use erloff, only: fatal_t
+    use iso_varying_string, only: char
+    use rojff_json_string_m, only: json_string_unsafe
+    use rojff_json_value_m, only: json_value_t
+    use rojff_parser_m, only: parse_json_string
+    use rojff_string_cursor_m, only: string_cursor_t
+    use rojff_utils_m, only: INVALID_INPUT
+
+    implicit none
+
+    character (len=*), parameter :: MODULE_NAME = "rojff_fallible_json_string_m"
+contains
+    module procedure from_character
+        character(len=*), parameter :: PROCEDURE_NAME = "from_character"
+        type(string_cursor_t) :: cursor
+        class(json_value_t), allocatable :: json
+
+        cursor = string_cursor_t(string // '"')
+        call parse_json_string(cursor, json, fallible_string%errors)
+        if (fallible_string%errors%has_any()) then
+            fallible_string%errors = error_list_t( &
+                    fallible_string%errors, &
+                    module_t(MODULE_NAME), &
+                    procedure_t(PROCEDURE_NAME))
+        else if (.not.cursor%finished()) then
+            fallible_string%errors = error_list_t(fatal_t( &
+                    INVALID_INPUT, &
+                    module_t(MODULE_NAME), &
+                    procedure_t(PROCEDURE_NAME), &
+                    "Unescaped quote in string: '" // string // "'"))
+        else
+            fallible_string%string = json_string_unsafe(string)
+        end if
+    end procedure
+
+    module procedure from_string
+        fallible_string = fallible_json_string_t( &
+                fallible_json_string_t(char(string)), &
+                module_t(MODULE_NAME), &
+                procedure_t("from_string"))
+    end procedure
+
+    module procedure from_json_string
+        fallible_string%string = string
+    end procedure
+
+    module procedure from_fallible_string
+        if (maybe_string%failed()) then
+            fallible_string%errors = error_list_t( &
+                    maybe_string%errors, module_, procedure_)
+        else
+            fallible_string%string = maybe_string%string
+        end if
+    end procedure
+
+    module procedure failed
+        failed = self%errors%has_any()
+    end procedure
+end submodule
