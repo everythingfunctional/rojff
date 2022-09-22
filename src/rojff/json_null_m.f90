a9e9ed9a22882eb1225c3679c784442779a09d78
Brad Richardson <everythingfunctional@protonmail.com>
2022-09-21 19:05:38 -0500

    refac(json_null): split into module and submodule

--- src/rojff/json_null_m.f90
+++ src/rojff/json_null_m.f90
@@ -12,42 +12,31 @@ module rojff_json_null_m
         procedure :: write_to_compactly
         procedure :: write_to_expanded
     end type
-contains
-    subroutine create_json_null(json)
-        class(json_value_t), allocatable, intent(out) :: json
 
-        allocate(json_null_t :: json)
-    end subroutine
+    interface
+        module subroutine create_json_null(json)
+            implicit none
+            class(json_value_t), allocatable, intent(out) :: json
+        end subroutine
 
-    elemental function equals(lhs, rhs)
-        class(json_null_t), intent(in) :: lhs
-        class(json_value_t), intent(in) :: rhs
-        logical :: equals
+        elemental module function equals(lhs, rhs)
+            implicit none
+            class(json_null_t), intent(in) :: lhs
+            class(json_value_t), intent(in) :: rhs
+            logical :: equals
+        end function
 
-        associate(unused => lhs); end associate
-        select type (rhs)
-        type is (json_null_t)
-            equals = .true.
-        class default
-            equals = .false.
-        end select
-    end function
+        module subroutine write_to_compactly(self, sink)
+            implicit none
+            class(json_null_t), intent(in) :: self
+            class(string_sink_t), intent(inout) :: sink
+        end subroutine
 
-    subroutine write_to_compactly(self, sink)
-        class(json_null_t), intent(in) :: self
-        class(string_sink_t), intent(inout) :: sink
-
-        associate(unused => self); end associate
-        call sink%append("null")
-    end subroutine
-
-    subroutine write_to_expanded(self, indentation_level, sink)
-        class(json_null_t), intent(in) :: self
-        integer, intent(in) :: indentation_level
-        class(string_sink_t), intent(inout) :: sink
-
-        associate(unused => indentation_level); end associate
-        associate(unused => self); end associate
-        call sink%append("null")
-    end subroutine
+        module subroutine write_to_expanded(self, indentation_level, sink)
+            implicit none
+            class(json_null_t), intent(in) :: self
+            integer, intent(in) :: indentation_level
+            class(string_sink_t), intent(inout) :: sink
+        end subroutine
+    end interface
 end module
---
+++ src/rojff/json_null_s.f90
@@ -0,0 +1,28 @@
+submodule(rojff_json_null_m) rojff_json_null_s
+    implicit none
+contains
+    module procedure create_json_null
+        allocate(json_null_t :: json)
+    end procedure
+
+    module procedure equals
+        associate(unused => lhs); end associate
+        select type (rhs)
+        type is (json_null_t)
+            equals = .true.
+        class default
+            equals = .false.
+        end select
+    end procedure
+
+    module procedure write_to_compactly
+        associate(unused => self); end associate
+        call sink%append("null")
+    end procedure
+
+    module procedure write_to_expanded
+        associate(unused => indentation_level); end associate
+        associate(unused => self); end associate
+        call sink%append("null")
+    end procedure
+end submodule
