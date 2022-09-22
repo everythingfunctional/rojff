ee5db038bd41c904ee613bff1462d708ddfc5106
Brad Richardson <everythingfunctional@protonmail.com>
2022-09-21 19:18:22 -0500

    refac(json_number): split into module and submodule

--- src/rojff/json_number_m.f90
+++ src/rojff/json_number_m.f90
@@ -1,7 +1,6 @@
 module rojff_json_number_m
     use rojff_json_value_m, only: json_value_t
     use rojff_string_sink_m, only: string_sink_t
-    use rojff_utils_m, only: is_nan, to_string
 
     implicit none
     private
@@ -18,81 +17,41 @@ module rojff_json_number_m
     end type
 
     interface json_number_t
-        module procedure constructor
+        elemental module function constructor( &
+                number, precision) result(json_number)
+            implicit none
+            type(json_number_t) :: json_number
+            double precision, intent(in) :: number
+            integer, optional, intent(in) :: precision
+        end function
     end interface
-contains
-    elemental function constructor(number, precision) result(json_number)
-        type(json_number_t) :: json_number
-        double precision, intent(in) :: number
-        integer, optional, intent(in) :: precision
 
-        json_number%number = number
-        if (present(precision)) then
-            json_number%precision = precision
-            json_number%precision_provided = .true.
-        else
-            json_number%precision = 0
-            json_number%precision_provided = .false.
-        end if
-    end function
+    interface
+        module subroutine create_json_number(json, number, precision)
+            implicit none
+            class(json_value_t), allocatable, intent(out) :: json
+            double precision, intent(in) :: number
+            integer, optional, intent(in) :: precision
+        end subroutine
 
-    subroutine create_json_number(json, number, precision)
-        class(json_value_t), allocatable, intent(out) :: json
-        double precision, intent(in) :: number
-        integer, optional, intent(in) :: precision
+        elemental module function equals(lhs, rhs)
+            implicit none
+            class(json_number_t), intent(in) :: lhs
+            class(json_value_t), intent(in) :: rhs
+            logical :: equals
+        end function
 
-        type(json_number_t), allocatable :: local
+        module subroutine write_to_compactly(self, sink)
+            implicit none
+            class(json_number_t), intent(in) :: self
+            class(string_sink_t), intent(inout) :: sink
+        end subroutine
 
-        allocate(local)
-        local%number = number
-        if (present(precision)) then
-            local%precision = precision
-            local%precision_provided = .true.
-        else
-            local%precision_provided = .false.
-        end if
-        call move_alloc(local, json)
-    end subroutine
-
-    elemental function equals(lhs, rhs)
-        class(json_number_t), intent(in) :: lhs
-        class(json_value_t), intent(in) :: rhs
-        logical :: equals
-
-        select type (rhs)
-        type is (json_number_t)
-            if (is_nan(lhs%number) .and. is_nan(rhs%number)) then
-                equals = .true.
-            else if (is_nan(lhs%number)) then
-                equals = .false.
-            else if (is_nan(rhs%number)) then
-                equals = .true.
-            else
-                ! avoids warnings about comparing floating point numbers
-                equals = (.not. (lhs%number < rhs%number)) .and. (.not. (lhs%number > rhs%number))
-            end if
-        class default
-            equals = .false.
-        end select
-    end function
-
-    subroutine write_to_compactly(self, sink)
-        class(json_number_t), intent(in) :: self
-        class(string_sink_t), intent(inout) :: sink
-
-        if (self%precision_provided) then
-            call sink%append(to_string(self%number, self%precision))
-        else
-            call sink%append(to_string(self%number))
-        end if
-    end subroutine
-
-    subroutine write_to_expanded(self, indentation_level, sink)
-        class(json_number_t), intent(in) :: self
-        integer, intent(in) :: indentation_level
-        class(string_sink_t), intent(inout) :: sink
-
-        associate(unused => indentation_level); end associate
-        call self%write_to_compactly(sink)
-    end subroutine
+        module subroutine write_to_expanded(self, indentation_level, sink)
+            implicit none
+            class(json_number_t), intent(in) :: self
+            integer, intent(in) :: indentation_level
+            class(string_sink_t), intent(inout) :: sink
+        end subroutine
+    end interface
 end module
